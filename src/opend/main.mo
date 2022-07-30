11fdad19ce3e6601280511464fc05008c6f52156
Parth Rathod <parth.rathod12@gmail.com>
2022-07-30 18:38:15 +0530

    Display-owned-NFTs

--- src/opend/main.mo
+++ src/opend/main.mo
@@ -2,8 +2,13 @@ import Cycles "mo:base/ExperimentalCycles";
 import Principal "mo:base/Principal";
 import NFTActorClass "../NFT/nft";
 import Debug "mo:base/Debug";
+import HashMap "mo:base/HashMap";
+import List "mo:base/List";
 
 actor OpenD {
+
+    var mapOfNFTs = HashMap.HashMap<Principal, NFTActorClass.NFT>(1, Principal.equal, Principal.hash);
+    var mapOfOwners = HashMap.HashMap<Principal, List.List<Principal>>(1, Principal.equal, Principal.hash);
 
     public shared(msg) func mint(imgData: [Nat8], name : Text) : async Principal {
         let owner : Principal = msg.caller;
@@ -17,7 +22,30 @@ actor OpenD {
 
         let newNFTPrincipal = await newNFT.getCanisterId();
 
+        mapOfNFTs.put(newNFTPrincipal, newNFT);
+        addToOwnershipMap(owner, newNFTPrincipal);
+
         return newNFTPrincipal; 
-    }
+    };
+
+    private func addToOwnershipMap(owner: Principal, nftId: Principal) {
+        var ownedNFTs: List.List<Principal> = switch(mapOfOwners.get(owner)){
+            case null List.nil<Principal>();
+            case (?result) result;
+        };
+
+        ownedNFTs := List.push(nftId, ownedNFTs);
+        mapOfOwners.put(owner, ownedNFTs);
+    };
+
+    public query func getOwnedNFTs(user: Principal) : async [Principal]{
+
+        var userNFTs: List.List<Principal> = switch (mapOfOwners.get(user)) {
+            case null List.nil<Principal>();
+            case (?result) result;
+        };
+
+        return List.toArray(userNFTs);
+    };
 
 };
--- src/opend_assets/src/components/Header.jsx
+++ src/opend_assets/src/components/Header.jsx
@@ -1,29 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets/logo.png";
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import homeImage from "../../assets/home-img.png";
+import Minter from "./Minter";
+import Gallery from "./Gallery";
+import {opend} from "../../../declarations/opend";
+import CURRENT_USER_ID from "../index";
 
 function Header() {
+
+  const [userOwnedGallery, setOwnedGallary] = useState();
+
+  async function getNFTs() {
+   const userNFTIds =  await opend.getOwnedNFTs(CURRENT_USER_ID);
+   setOwnedGallary(<Gallery title="My NFTs" ids= {userNFTIds} />);
+   console.log(userNFTIds);
+  }
+
+  useEffect(() => {
+    getNFTs();
+  }, [])
+
   return (
+    <BrowserRouter forceRefresh={true}>
     <div className="app-root-1">
       <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
         <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
           <div className="header-left-4"></div>
           <img className="header-logo-11" src={logo} />
           <div className="header-vertical-9"></div>
-          <h5 className="Typography-root header-logo-text">OpenD</h5>
+          <h5 className="Typography-root header-logo-text">
+            <Link to={"/"}>
+            OpenD
+            </Link>
+            </h5>
           <div className="header-empty-6"></div>
           <div className="header-space-8"></div>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+            <Link to= {"/discover"}>
             Discover
+            </Link>
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+            <Link to= {"/minter"}>
             Minter
+            </Link>
           </button>
           <button className="ButtonBase-root Button-root Button-text header-navButtons-3">
+            <Link to= {"/collection"}>
             My NFTs
+            </Link>
           </button>
         </div>
       </header>
     </div>
+    <Switch>
+      <Route path="/discover">
+      <h1> Discover</h1>
+      </Route>
+      <Route path= "/minter">
+        <Minter />
+      </Route>
+      <Route path= "/collection">
+       {userOwnedGallery}
+      </Route>
+      <Route path="/">
+      <img className="bottom-space" src={homeImage} />
+      </Route>
+    </Switch>
+    </BrowserRouter>
   );
 }
 
